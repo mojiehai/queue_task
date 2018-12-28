9552d11ac17c4a32eb90deab9f46dc7eb61b10d1
psh <pengshuhai@jybdshop.cn>
2018-12-28 19:06:39 +0800

    add file lock

--- tests/config.php
+++ tests/config.php
@@ -7,7 +7,7 @@ return [
                 'DB_HOST'       => '127.0.0.1',     //数据库地址
                 'DB_PORT'       => '3306',          //数据库端口
                 'DB_USERNAME'   => 'root',          //用户名
-                'DB_PASSWORD'   => 'Merror.2017',   //密码
+                'DB_PASSWORD'   => 'root',          //密码
                 'DB_CHARSET'    => 'utf8',          //数据库字符集
                 'DB_DATABASE'   => 'queue_task',    //数据库名称
                 'DB_TABLE'      => 'job_queue',     //表名称
